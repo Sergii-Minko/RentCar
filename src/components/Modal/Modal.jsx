1757ad086aa4e18863eca51817f7b256da919921
Sergii Minko <minkosergii@gmail.com>
2024-06-16 12:15:37 +0200

    modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,18 +5,57 @@ const Modal = ({ isOpen, onClose, car }) => {
   if (!isOpen) return null;
 
   const {
+    id,
+    img,
     model,
     make,
     year,
     rentalPrice,
     address,
     rentalCompany,
+    description,
     type,
     mileage,
     functionalities,
+    accessories,
+    rentalConditions,
   } = car;
 
   const [street, city, country] = address.split(",").map((part) => part.trim());
+
+  const combined = [
+    ...accessories.map((item) => item),
+    ...functionalities.map((item) => item),
+  ];
+
+  const combinedString = combined.join(" | ");
+
+  const rentalConditionsArray = rentalConditions
+    .split("\n")
+    .map((part) => part.trim());
+
+  const rentalConditionsItemsSpan = rentalConditionsArray.map((item, index) => {
+    if (item.includes(":")) {
+      const parts = item.split(":");
+      const firstPart = parts[0].trim();
+      const secondPart = parts[1].trim();
+      return (
+        <React.Fragment key={index}>
+          {firstPart}: <span className={css.span}>{secondPart}</span>
+        </React.Fragment>
+      );
+    } else {
+      return <React.Fragment key={index}>{item}</React.Fragment>;
+    }
+  });
+
+  const rentalConditionsString = rentalConditionsItemsSpan.map(
+    (element, index) => (
+      <React.Fragment key={index}>
+        {index > 0 && " | "} {element}
+      </React.Fragment>
+    )
+  );
 
   return (
     <div className={css.modalOverlay} onClick={onClose}>
@@ -25,9 +64,24 @@ const Modal = ({ isOpen, onClose, car }) => {
           &times;
         </button>
         <div className={css.modalBody}>
-          <h2>
-            {make} {model}, {year}
-          </h2>
+          <div className={css.itemImg}>
+            <img src={img} alt={model} height="248" />
+          </div>
+          <div className={css.infoDiv}>
+            <h2 className={css.carTitle}>
+              {make} <span className={css.model}>{model}</span>, {year}
+            </h2>
+          </div>
+          <p className={css.carDetails}>
+            {city} | {country} | {rentalCompany} | {type} | {model} | {mileage}{" "}
+            | {functionalities[0]}
+          </p>
+          <p> {description}</p>
+          <h2 className={css.TitleFunc}> Accessories and functionalities:</h2>
+          <p> {combinedString}</p>
+
+          <h2 className={css.TitleFunc}>Rental Conditions:</h2>
+          <p> {rentalConditionsString}</p>
           <p>Rental Price: {rentalPrice}</p>
           <p>
             Location: {city}, {country}
@@ -35,7 +89,6 @@ const Modal = ({ isOpen, onClose, car }) => {
           <p>Rental Company: {rentalCompany}</p>
           <p>Type: {type}</p>
           <p>Mileage: {mileage}</p>
-          <p>Functionalities: {functionalities.join(", ")}</p>
         </div>
       </div>
     </div>
